f9310cad7eb03c4b709d78a006a8f1384bd63d4e
ssdesth <carlosmontt19@hotmail.com>
2020-11-03 20:24:00 -0600

    Updates

---
+++ precache-manifest.226b74614222e03c25e277bdf118e0a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2811e9655432a971d1a79a0cf1bc2ca6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "045abf7251f3573ee493",
+    "url": "/monsters-rolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045abf7251f3573ee493",
+    "url": "/monsters-rolodex/static/js/main.fd99c1a8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
